f8310568da3ec4e0d86f309e33c27f8a0dc890ec
ballm06m06 <ballm06m06@yahoo.com.tw>
2020-08-25 18:50:57 +0800

    upload first solution

--- week1/Solution.java
+++ week1/Solution.java
@@ -1,3 +1,9 @@
+/*
+Week 1 
+Qusetion: single number
+Description:
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+*/
 import java.util.Arrays;
 
 class Solution {
