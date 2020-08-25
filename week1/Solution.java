882327e67575460209fc6a0e9c91a6dd78828a9b
ballm06m06 <ballm06m06@yahoo.com.tw>
2020-08-25 18:34:51 +0800

    upload first solution

--- week1/Solution.java
+++ week1/Solution.java
@@ -1,36 +1,27 @@
-import java.util.HashMap;
 import java.util.Arrays;
 
 class Solution {
     public static void main(String[] args){
-		int[] intArray = new int[]{1,2,2};
+		int[] intArray = new int[]{1,1,2,2,4};
 		Solution s = new Solution();
 		s.singleNumber(intArray);
 	} 
 	
-	
 	public int singleNumber(int[] nums) {          
         Arrays.sort(nums);
         
-        int theone = 0;
+        int theOne = -100;
         
-        for(int i = 0; i<nums.length/2 - 1; i++){
-            if(nums[i]!=nums[i+2] && nums[i]==nums[i+1]){
-                System.out.println("fuck you");
-                continue;
-            }else{
-                System.out.println("Single number: "+nums[i]);
-                theone = nums[i];
-            }        
+        for(int i = 0; i < nums.length- 1; i+=2){
+            if(nums[i]!=nums[i+1]){
+                theOne = nums[i];
+				break;
+            }    
         }
-         System.out.println("hi");
-        return theone;
-        
-        // HashMap<String, String> Numbers = new HashMap<String, String>();
-        // Numbers.put(nums[i])
+		if(theOne == -100){
+			theOne = nums[nums.length - 1];
+		}
+        return theOne;
     }
-
-	
-	
 }
 
