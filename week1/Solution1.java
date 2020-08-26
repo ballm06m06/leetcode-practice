31583a06a7f873c945fda6fb4b28982da967e657
ballm06m06 <ballm06m06@yahoo.com.tw>
2020-08-26 22:36:28 +0800

    upload Solution1.java

---
+++ week1/Solution1.java
@@ -0,0 +1,39 @@
+/*
+ * Week 1
+ * Author: @ballm06m06
+ * Qusetion: Happy Number
+ * Description: A happy number is a number defined by the following process: Starting with any positive integer, 
+ *              replace the number by the sum of the squares of its digits, and repeat the process until the number 
+ *              equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+ *              Those numbers for which this process ends in 1 are happy numbers.
+*/
+
+import java.util.HashSet;
+
+class Solution1{
+    public boolean isHappy(int n){
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        
+        /* 
+         * n != 1 means it's not a happy number
+        */
+        while(n != 1){ 
+            /*why should we return flase??? */
+            if(hashSet.contains(n)){
+                return false;
+            }
+            
+            hashSet.add(n);
+            int total = 0;
+            /* 
+            * Break down the number into individual digit.
+            */
+            while(n!=0){           
+                total = total + (n % 10)*(n % 10);
+                n = n / 10;
+            }
+            n = total;
+        }
+        return true;
+    }
+}
---
+++ week1/Solution_1.java
@@ -0,0 +1,38 @@
+/*
+ * Week 1
+ * Author: @ballm06m06
+ * Qusetion: Single Number
+ * Description: Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+*/
+
+import java.util.HashMap;
+
+class Solution_1{
+    public int findSingleNumber(int[] nums){
+        HashMap<Integer, Integer> hashTable = new HashMap<Integer, Integer>();
+
+        //put all elements into HashMap
+        for(int i : nums){
+            /*
+            *  getOrDefault(key, defaultValue)
+            *  Description: Returns the value to which the specified key is mapped, or defaultValue if this map contains no mapping for the key.
+            *  Explanation: First timee call this getOrDefault(i, 0)+1 ,cuz we don't have values, it'll return default value;
+            *               Next time we call this function, its value is 1+1 = 2
+            */ 
+            hashTable.put(i, hashTable.getOrDefault(i, 0)+1);
+        }
+
+        for(int i : nums){
+            if(hashTable.get(i) == 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] list = {1,2,2,4,4,1,5};
+        Solution_1 s = new Solution_1();
+        System.out.println(s.findSingleNumber(list));
+    }
+}
